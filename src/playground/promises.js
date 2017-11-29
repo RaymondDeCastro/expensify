4eda2ff49636230411579a2f52483bbbd4d5784e
Raymond De Castro <glifter8@gmail.com>
2017-11-28 22:13:21 -0800

    Setup test database env

---
+++ src/playground/promises.js
@@ -0,0 +1,30 @@
+import { setTimeout } from "timers";
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {  
+    resolve({
+      name: 'Raymond',
+      age: 32
+    });
+    // reject('Something went wrong');
+  }, 5000);
+});
+
+console.log('before');
+
+promise.then((data) => {
+  console.log('1', data);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {  
+      resolve('this is my other promise');
+      // reject('Something went wrong');
+    }, 5000);
+  });
+}).then((str) => {
+  console.log('does this run?', str);
+}).catch((error) => {
+  console.log('error: ', error);
+});
+
+console.log('after');
