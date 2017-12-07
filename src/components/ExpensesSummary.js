645df64d13c35ca231334744eeaa19d9a9e57fcc
Raymond De Castro <glifter8@gmail.com>
2017-12-07 06:25:19 -0800

    Styled input areas. Just need to style expense list now

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -13,7 +13,7 @@ export const ExpensesSummary = ({ expenseCount, totalAmount }) => {
     <div className="page-header">
       <div className="content-container">
         <h1 className="page-header__title">
-          Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span>
+          Viewing <span>{expenseCount}</span> {expenseWord} totalling: <span>{formattedExpensesTotal}</span>
         </h1>
         <div className="page-header__actions">
           <Link className="button" to="/create">Add Expense</Link>
